3778ff363f967b5273f391f243eb92d90a599b23
etsibareva <etsibareva.university@gmail.com>
2026-04-30 09:40:48 +0300

    init rename

--- tasks/tsibareva_e_integral_calculate_trapezoid_method/tests/performance/main.cpp
+++ tasks/tsibareva_e_integral_calculate_trapezoid_method/tests/performance/main.cpp
@@ -7,6 +7,7 @@
 #include "tsibareva_e_integral_calculate_trapezoid_method/omp/include/ops_omp.hpp"
 #include "tsibareva_e_integral_calculate_trapezoid_method/seq/include/ops_seq.hpp"
 #include "tsibareva_e_integral_calculate_trapezoid_method/tbb/include/ops_tbb.hpp"
+#include "tsibareva_e_integral_calculate_trapezoid_method/stl/include/ops_stl.hpp"
 #include "util/include/perf_test_util.hpp"
 
 namespace tsibareva_e_integral_calculate_trapezoid_method {
@@ -40,7 +41,8 @@ namespace {
 
 const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<Integral, TsibarevaEIntegralCalculateTrapezoidMethodSEQ,
                                                        TsibarevaEIntegralCalculateTrapezoidMethodOMP,
-                                                       TsibarevaEIntegralCalculateTrapezoidMethodTBB>(
+                                                       TsibarevaEIntegralCalculateTrapezoidMethodTBB,
+                                                       TsibarevaEIntegralCalculateTrapezoidMethodSTL>(
     PPC_SETTINGS_tsibareva_e_integral_calculate_trapezoid_method);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
